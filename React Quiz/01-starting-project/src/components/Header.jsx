7f9dcbb5e328eee0f4264d770281e3d7acc23cf6
DELL <farhan.irfan@braincrop.net>
2025-05-30 17:39:35 +0500

    feat: add header

---
+++ React Quiz/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="header">
+    <img src="./src/assets/quiz-logo.png" alt="Quiz Logo" className="header--image" />
+      <h1>React Quiz</h1>
+    </header>
+  )
+}
+
+export default Header
